b0bc8acf7ff905d2108ae71c540c8b0d9aa061cf
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-25 13:11:19 +0200

    Navigate with useEffect instead of when component first renders

--- src/components/LoginRedirect.js
+++ src/components/LoginRedirect.js
@@ -1,18 +1,22 @@
 import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { tryLogin } from "../authHelpers";
 import useAuth from "../useAuth";
 
 function LoginRedirect({}) {
   const { login, loading, error } = useAuth();
   const nav = useNavigate();
-  
+
   useEffect(() => {
     login();
   }, [nav, login, error]);
 
+  useEffect(() => {
+    if (loading) return;
+    else nav("/");
+  }, [loading, nav]);
+
   if (loading) return <p>Logging in...</p>;
-  else nav("/login");
+  else return null;
 }
 
 export default LoginRedirect;
