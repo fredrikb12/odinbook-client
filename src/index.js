928958bc9538dec4a2b65ec201253f138a6bcba3
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-25 15:24:18 +0200

    Add global generic styling

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import RouteSwitch from "./components/RouteSwitch";
+import { GlobalStyle } from "./components/styled/GlobalStyle";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+    <GlobalStyle />
     <RouteSwitch />
   </React.StrictMode>
 );
