e720f4d5d1cfefb6c54cd0c72018ebc327825d64
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-25 15:51:25 +0200

    Add theme provider

--- src/components/RouteSwitch.js
+++ src/components/RouteSwitch.js
@@ -1,38 +1,47 @@
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
+import { ThemeProvider } from "styled-components";
 import App from "../App";
 import useAuth, { AuthProvider } from "../useAuth";
 import Homepage from "./Homepage";
 import Login from "./Login";
 import LoginRedirect from "./LoginRedirect";
 import Profile from "./Profile";
+import { GlobalStyle } from "./styled/GlobalStyle";
 
 function RouteSwitch() {
+  const theme = {
+    main: "#18191A",
+    postBg: "#242526",
+  };
   return (
     <BrowserRouter>
-      <AuthProvider>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route
-              index
-              element={
-                <AuthRoute>
-                  <Homepage />
-                </AuthRoute>
-              }
-            />
-            <Route path="/login" element={<Login />} />
-            <Route path="/login-redirect" element={<LoginRedirect />} />
-            <Route
-              path="/profile/:profileId"
-              element={
-                <AuthRoute>
-                  <Profile />
-                </AuthRoute>
-              }
-            />
-          </Route>
-        </Routes>
-      </AuthProvider>
+      <ThemeProvider theme={theme}>
+        <AuthProvider>
+          <GlobalStyle />
+          <Routes>
+            <Route path="/" element={<App />}>
+              <Route
+                index
+                element={
+                  <AuthRoute>
+                    <Homepage />
+                  </AuthRoute>
+                }
+              />
+              <Route path="/login" element={<Login />} />
+              <Route path="/login-redirect" element={<LoginRedirect />} />
+              <Route
+                path="/profile/:profileId"
+                element={
+                  <AuthRoute>
+                    <Profile />
+                  </AuthRoute>
+                }
+              />
+            </Route>
+          </Routes>
+        </AuthProvider>
+      </ThemeProvider>
     </BrowserRouter>
   );
 }
