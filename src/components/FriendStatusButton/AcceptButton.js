3e9137bc3f75dec05ede6d54496deca2f5cfa133
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-27 16:37:34 +0200

    Accept friend request on click

--- src/components/FriendStatusButton/AcceptButton.js
+++ src/components/FriendStatusButton/AcceptButton.js
@@ -1,6 +1,9 @@
-function AcceptButton({ currentUser, user, setNeedsUpdate }) {
+import {friendActions} from "../../utils/friendStatus";
+
+function AcceptButton({ req, currentUser, user, setNeedsUpdate }) {
   async function handleAccept() {
     console.log("accepting...");
+    const res = await friendActions.handleRequest(req._id, user, "accepted")
     setNeedsUpdate(true);
   }
   return <button onClick={handleAccept}>Accept</button>;
