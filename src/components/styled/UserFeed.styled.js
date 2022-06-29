0f06f09add0d9392f12785343e6e2ff2ef53ef1a
fredrikb12 <fredrik.backman12@gmail.com>
2022-06-29 15:11:07 +0200

    Change to single-column layout on small screens

--- src/components/styled/UserFeed.styled.js
+++ src/components/styled/UserFeed.styled.js
@@ -2,11 +2,11 @@ import styled from "styled-components";
 import { StyledPostFeed } from "./PostFeed.styled";
 
 export const StyledUserFeed = styled(StyledPostFeed)`
-  gap: 10px;
   background-color: ${({ theme }) => theme.postBg};
   padding: 20px;
   margin-top: 0;
   min-height: 90vh;
+  font-size: 1.1rem;
 
   & > div {
     display: flex;
@@ -16,5 +16,12 @@ export const StyledUserFeed = styled(StyledPostFeed)`
 
     box-shadow: 0px 0px 2px 0px rgba(255, 255, 255, 0.05);
     padding: 20px;
+  }
+
+  @media (min-width: 1024px) {
+    display: grid;
+    grid-template-columns: 1fr 1fr;
+    grid-auto-rows: 200px;
+    gap: 20px;
   }
 `;
