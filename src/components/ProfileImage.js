4d9428ea343f59e9755c3a2f1c626a03466dcf20
fredrikb12 <fredrik.backman12@gmail.com>
2022-07-08 11:28:39 +0200

    Add default profile image

--- src/components/ProfileImage.js
+++ src/components/ProfileImage.js
@@ -1,8 +1,10 @@
+import defaultProfile from "../images/profile-default.svg";
+
 function ProfileImage({ src, alt }) {
   return (
     <img
       style={{ borderRadius: "50%", width: "50px", height: "50px" }}
-      src={src || "https://picsum.photos/50/50"}
+      src={src || defaultProfile}
       alt={alt}
     />
   );
